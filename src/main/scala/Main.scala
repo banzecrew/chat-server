198b6896c980688b2e59da405b6d032f69c5e36d
hex <dkrykovwork@gmail.com>
2022-05-15 19:31:13 +0400

    chore: givens instead implicits

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,20 +6,22 @@ import akka.http.scaladsl.model.ws.{ TextMessage, Message }
 import akka.http.scaladsl.server.Directives.*
 import akka.stream.scaladsl.{ Source, Flow }
 import scala.io.StdIn
+import scala.concurrent.ExecutionContextExecutor
 
 object HttpServerRoutingMinimal:
 
   def main(args: Array[String]): Unit =
 
-    implicit val system = ActorSystem(Behaviors.empty, "my-system")
-    implicit val executionContext = system.executionContext
+    given system: ActorSystem[Any] = ActorSystem(Behaviors.empty, "my-system")
+    given executionContext: ExecutionContextExecutor = system.executionContext
 
     val (addr, port) = ("localhost", 8080)
 
     val greeter = 
       Flow[Message]
         .collect {
-          case tm: TextMessage => TextMessage(Source.single("Hello ") ++ tm.textStream)
+          case tm: TextMessage =>
+            TextMessage(Source.single("Hello ") ++ tm.textStream)
         }
       
     val route =
